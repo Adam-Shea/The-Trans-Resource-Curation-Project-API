e6ead9065b8377affa0f34e01376d13692a576fa
Adam <davlyn.sheaa@outlook.com>
2022-04-20 00:46:37 +0100

    fixed sequelize+mysql connectivity

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,25 +1,30 @@
 import { Options, Sequelize } from 'sequelize'
+import dotenv from "dotenv"
+dotenv.config()
 
 import article from './article'
 
 // Open database connection
-// const sequelize = new Sequelize(
-//     config.database.database,
-//     config.database.username,
-//     config.database.password,
-//     <Options>config.database
-// )
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE!,
+    process.env.DB_USERNAME!,
+    process.env.DB_PASSWORD!,
+    {
+        host: process.env.DB_HOST!,
+        dialect: 'mysql',
+    }
+)
 
 // Initialize each model in the database
 // This must be done before associations are made
 let models = [article]
-// models.forEach(model => model.initialize(sequelize))
+models.forEach(model => model.initialize(sequelize))
 
-// // Create database tables
-// //   force: true causes database to reset with each run
-// sequelize.sync({ force: true })
+// Create database tables
+//   force: true causes database to reset with each run
+sequelize.sync({ force: true })
 
 export {
-    //sequelize as Database,
+    sequelize as Database,
     article
 }

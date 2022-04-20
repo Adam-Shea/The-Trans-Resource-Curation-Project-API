644f0f770565c1b0db18fee9cbd4eb562a692850
Adam <davlyn.sheaa@outlook.com>
2022-04-20 22:27:50 +0100

    enabled cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
+var cors = require('cors');
 
 import articleRoutes from './article'
 
 const router = Router()
-
+router.use(cors())
 router.use('/article', articleRoutes)
+
 
 export default router

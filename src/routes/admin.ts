ae17142acf0bd3a33f2843ee3f93cf842abc3efb
Adam <davlyn.sheaa@outlook.com>
2022-04-25 22:28:14 +0100

    catch error

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -7,78 +7,106 @@ const router = Router()
 
 router.route('/articles')
     .get((req, res, next) => {
-        if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
-            article.getArticles()
-                .then(response => res.status(200).send(response))
-                .finally(next)
-        } else {
-            res.sendStatus(401)
+        try {
+            if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
+                article.getArticles()
+                    .then(response => res.status(200).send(response))
+                    .finally(next)
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
         }
     })
 
 router.route('/articles/:id')
     .put((req, res, next) => {
-        if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
-            article.updateArticle(parseInt(req.params.id), req.body)
-                .then(response => res.sendStatus(response))
-                .finally(next)
-        } else {
-            res.sendStatus(401)
+        try {
+            if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
+                article.updateArticle(parseInt(req.params.id), req.body)
+                    .then(response => res.sendStatus(response))
+                    .finally(next)
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
         }
     })
 
 router.route('/articles/:id')
     .delete((req, res, next) => {
-        if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
-            article.deleteArticle(parseInt(req.params.id),)
-                .then(response => res.sendStatus(response))
-                .finally(next)
-        } else {
-            res.sendStatus(401)
+        try {
+            if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
+                article.deleteArticle(parseInt(req.params.id),)
+                    .then(response => res.sendStatus(response))
+                    .finally(next)
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
         }
     })
 
 router.route('/articles')
     .post((req, res, next) => {
-        if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
-            article.createArticle(req.body)
-                .then(response => res.sendStatus(response))
-                .finally(next)
-        } else {
-            res.sendStatus(401)
+        try {
+            if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
+                article.createArticle(req.body)
+                    .then(response => res.sendStatus(response))
+                    .finally(next)
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
         }
     })
 
 router.route('/categories/:id')
     .delete((req, res, next) => {
-        if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
-            category.deleteCategory(parseInt(req.params.id))
-                .then(response => res.sendStatus(response))
-                .finally(next)
-        } else {
-            res.sendStatus(401)
+        try {
+            if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
+                category.deleteCategory(parseInt(req.params.id))
+                    .then(response => res.sendStatus(response))
+                    .finally(next)
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
         }
     })
 
 router.route('/categories/:id')
     .put((req, res, next) => {
-        if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
-            category.updateCategory(parseInt(req.params.id), req.body)
-                .then(response => res.sendStatus(response))
-                .finally(next)
-        } else {
-            res.sendStatus(401)
+        try {
+            if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
+                category.updateCategory(parseInt(req.params.id), req.body)
+                    .then(response => res.sendStatus(response))
+                    .finally(next)
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
         }
     })
 
 router.route('/categories')
     .post((req, res, next) => {
-        if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
-            category.createCategory(req.body)
-                .then(response => res.sendStatus(response))
-                .finally(next)
-        } else {
-            res.sendStatus(401)
+        try {
+            if (twofactor.verifyToken(process.env.ADMIN_TOKEN!, req.headers.token)) {
+                category.createCategory(req.body)
+                    .then(response => res.sendStatus(response))
+                    .finally(next)
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
         }
     })
 

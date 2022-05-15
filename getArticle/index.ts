5754e05c43660d2eab72d346509b97b4f209ef44
Adam Shea <davlyn.sheaa@outlook.com>
2022-05-15 23:23:15 +0100

    fixed sql queries

--- getArticle/index.ts
+++ getArticle/index.ts
@@ -1,7 +1,14 @@
 module.exports = async function (context, input) {
     var documents = context.bindings.documents;
-    context.res = {
-        status: 200,
-        body: documents
-    };
+    if (documents.length > 0) {
+        context.res = {
+            status: 200,
+            body: documents
+        };
+    } else {
+        context.res = {
+            status: 404,
+            body: "No article found"
+        };
+    }
 };
